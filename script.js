e0e5641211408dd40492e7dca6adfb05bc3e7387
Baker <signupcat@protonmail.com>
2025-01-05 17:06:26 +1100

    Fix #8

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function fullscreen() {
     var el = document.getElementById("canvas");
-    el.requestFullscreen()
+
+    if (
+        document.fullscreenEnabled ||
+        document.mozFullScreenEnabled ||
+        document.documentElement.webkitRequestFullScreen
+    ) {
+        if (el.requestFullscreen) {
+            el.requestFullscreen();
+        } else if (el.mozRequestFullScreen) {
+            el.mozRequestFullScreen();
+        } else if (el.webkitRequestFullScreen) {
+            el.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+        }
+    }
 }
